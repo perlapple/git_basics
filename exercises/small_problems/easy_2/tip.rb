ebb3f4620f474529df7df220ba50fa0b54b815fc
Perla Garcia <perlapple@gmail.com>
2016-08-29 20:16:19 -0500

    tip exercise from small problems

---
+++ exercises/small_problems/easy_2/tip.rb
@@ -0,0 +1,13 @@
+
+
+puts "What is the bill? "
+bill = gets.chomp.to_f
+
+puts "What is the tip percentage?"
+percentage = gets.chomp.to_f
+
+tip = bill * percentage / 100
+total = bill + tip
+
+puts "The tip is #{tip}"
+puts "The total is #{total}"
