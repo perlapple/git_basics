f6fe0147142ddb161b0908ff90bb863ecba5cdbe
Perla Garcia <perlapple@gmail.com>
2016-08-29 20:53:25 -0500

    odd and even numbers small problems

---
+++ exercises/small_problems/easy_2/even_numbers.rb
@@ -0,0 +1,9 @@
+
+#Print all odd numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
+
+number = 1
+
+while number < 100
+  puts number if number.even?
+  number += 1
+end
