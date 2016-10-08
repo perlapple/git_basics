8147c2616d770c76b03af4c5de45b6f049646149
Perla Garcia <perlapple@gmail.com>
2016-10-08 13:20:46 -0500

    ROCK PAPER SCISSORS SPOCK LIZARD bonus feature

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,5 +1,6 @@
-#rock_paper_scissors.rb
-VALID_CHOISES = ['rock', 'paper', 'scissors']
+# rock_paper_scissors.rb
+
+VALID_CHOISES = %w(rock paper scissors spock lizard).freeze
 choice = nil
 
 def test_method
@@ -10,28 +11,35 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-puts test_method
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'spock' && second == 'rock')
+end
 
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-      (player == 'paper' && computer == 'rock') ||
-        (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt('You won!')
-  elsif (player == 'rock' && computer == 'paper') ||
-          (player == 'paper' && computer == 'scissors') ||
-            (player == 'scissors' && computer == 'rock')
-      prompt('Computer won!')
+  elsif win?(computer, player)
+    prompt('Computer won!')
   else
-      prompt('It\'s a tie!')
+    prompt('It\'s a tie!')
   end
 end
 
 loop do
   loop do
     prompt("Chosse one: #{VALID_CHOISES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    choice = gets.chomp
 
-    VALID_CHOISES.include?(choice) ? break : prompt('That is not a valid option')
+    VALID_CHOISES.include?(choice) ? break : prompt('Thats not a valid option')
   end
 
   computer_choice = VALID_CHOISES.sample
