e796d3e18229361393b7470252d3cfbac5d59bd2
Perla Garcia <perlapple@gmail.com>
2016-10-13 19:41:19 -0500

    rubocop modification tic tac toe

--- programing_foundations/lesson_4/tic_tac_toe.rb
+++ programing_foundations/lesson_4/tic_tac_toe.rb
@@ -1,30 +1,29 @@
 
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
 
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
 
-  [0,4,8],
-  [2,4,6]
-]
+  [0, 4, 8],
+  [2, 4, 6]
+].freeze
 
-@board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
+  puts '-----------'
   puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts "-----------"
+  puts '-----------'
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-def is_a_valid_position?(position)
-  position.to_s.to_i == position && position.between?(1, 9) && @board[position - 1] == " "
+def a_valid_position?(position)
+  position.to_s.to_i == position && position.between?(1, 9) && @board[position - 1] == ' '
 end
 
 def obtain_machine_position
@@ -45,14 +44,14 @@ def machine_mark(position)
 end
 
 def board_full?
-  @board.none?{ |value| value == ' ' }
+  @board.none? { |value| value == ' ' }
 end
 
 def winner?(mark)
   WIN_COMBINATIONS.select do |combination|
     if @board[combination[0]] == mark &&
-        @board[combination[1]] == mark &&
-        @board[combination[2]] == mark
+       @board[combination[1]] == mark &&
+       @board[combination[2]] == mark
       return true
     end
   end
@@ -64,27 +63,27 @@ def ask_position
   position = nil
 
   loop do
-    puts "Mark a square: (enter position 1 to 9 left to rigth)"
+    puts 'Mark a square: (enter position 1 to 9 left to rigth)'
     position = gets.chomp.to_i
-    break if is_a_valid_position?(position)
+    break if a_valid_position?(position)
   end
 
   position - 1
 end
 
-loop  do
+loop do
   loop do
     user_mark(ask_position)
     display_board(@board)
 
     if winner?('0')
-      puts "Congrats, you have won"
+      puts 'Congrats, you have won'
       break
     elsif winner?('X')
-      puts "Uh no, machine won"
+      puts 'Uh no, machine won'
       break
     elsif board_full?
-      puts "It's a tie!"
+      puts 'It\'s a tie!'
       break
     else
       machine_mark(obtain_machine_position)
